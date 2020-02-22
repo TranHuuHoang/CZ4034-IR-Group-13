79a024bb4adbab7d4e9599a9f8ee21d466f9332b
Hoang <thhoang1111999@gmail.com>
2020-02-22 23:18:39 +0800

    Reorder functions

--- search_engine/main.py
+++ search_engine/main.py
@@ -8,9 +8,66 @@ from bs4 import BeautifulSoup
 from elasticsearch import Elasticsearch
 
 
-def search(es_object, index_name, search):
-    res = es_object.search(index=index_name, body=search)
-    pprint(res)
+def connect_elasticsearch():
+    _es = None
+    _es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+    if _es.ping():
+        print('Yay Connected')
+    else:
+        print('Awww it could not connect!')
+    return _es
+
+
+def parse(u):
+    title = '-'
+    submit_by = '-'
+    description = '-'
+    calories = 0
+    ingredients = []
+    rec = {}
+
+    try:
+        r = requests.get(u, headers=headers)
+
+        if r.status_code == 200:
+            html = r.text
+            soup = BeautifulSoup(html, 'lxml')
+            # title
+            title_section = soup.select('.recipe-summary__h1')
+            # submitter
+            submitter_section = soup.select('.submitter__name')
+            # description
+            description_section = soup.select('.submitter__description')
+            # ingredients
+            ingredients_section = soup.select('.recipe-ingred_txt')
+
+            # calories
+            calories_section = soup.select('.calorie-count')
+            if calories_section:
+                calories = calories_section[0].text.replace('cals', '').strip()
+
+            if ingredients_section:
+                for ingredient in ingredients_section:
+                    ingredient_text = ingredient.text.strip()
+                    if 'Add all ingredients to list' not in ingredient_text and ingredient_text != '':
+                        ingredients.append({'step': ingredient.text.strip()})
+
+            if description_section:
+                description = description_section[0].text.strip().replace('"', '')
+
+            if submitter_section:
+                submit_by = submitter_section[0].text.strip()
+
+            if title_section:
+                title = title_section[0].text
+
+            rec = {'title': title, 'submitter': submit_by, 'description': description, 'calories': calories,
+                   'ingredients': ingredients}
+    except Exception as ex:
+        print('Exception while parsing')
+        print(str(ex))
+    finally:
+        return json.dumps(rec)
 
 
 def create_index(es_object, index_name):
@@ -73,66 +130,10 @@ def store_record(elastic_object, index_name, record):
         return is_stored
 
 
-def connect_elasticsearch():
-    _es = None
-    _es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-    if _es.ping():
-        print('Yay Connected')
-    else:
-        print('Awww it could not connect!')
-    return _es
+def search(es_object, index_name, search):
+    res = es_object.search(index=index_name, body=search)
+    pprint(res)
 
-
-def parse(u):
-    title = '-'
-    submit_by = '-'
-    description = '-'
-    calories = 0
-    ingredients = []
-    rec = {}
-
-    try:
-        r = requests.get(u, headers=headers)
-
-        if r.status_code == 200:
-            html = r.text
-            soup = BeautifulSoup(html, 'lxml')
-            # title
-            title_section = soup.select('.recipe-summary__h1')
-            # submitter
-            submitter_section = soup.select('.submitter__name')
-            # description
-            description_section = soup.select('.submitter__description')
-            # ingredients
-            ingredients_section = soup.select('.recipe-ingred_txt')
-
-            # calories
-            calories_section = soup.select('.calorie-count')
-            if calories_section:
-                calories = calories_section[0].text.replace('cals', '').strip()
-
-            if ingredients_section:
-                for ingredient in ingredients_section:
-                    ingredient_text = ingredient.text.strip()
-                    if 'Add all ingredients to list' not in ingredient_text and ingredient_text != '':
-                        ingredients.append({'step': ingredient.text.strip()})
-
-            if description_section:
-                description = description_section[0].text.strip().replace('"', '')
-
-            if submitter_section:
-                submit_by = submitter_section[0].text.strip()
-
-            if title_section:
-                title = title_section[0].text
-
-            rec = {'title': title, 'submitter': submit_by, 'description': description, 'calories': calories,
-                   'ingredients': ingredients}
-    except Exception as ex:
-        print('Exception while parsing')
-        print(str(ex))
-    finally:
-        return json.dumps(rec)
 
 
 if __name__ == '__main__':
