3a6a8b326c0288b2d4b3f44229925b64cc08c32d
Hoang <thhoang1111999@gmail.com>
2020-03-16 22:46:37 +0800

    Communication between flask server and elasticsearch database

--- Server/server.py
+++ Server/server.py
@@ -1,20 +1,29 @@
 from flask import Flask, render_template, url_for, jsonify, request, send_from_directory
 import requests
 
+import Query
+import Elastic
+
 app = Flask(__name__)
 
 
-@app.route("/search")
+@app.route("/home")
 @app.route("/")
-def search():
+def home():
     return render_template('search.html')
 
 
 @app.route("/ajax", methods = ['POST', 'GET'])
 def ajax():
     print(dict(request.values))
-    response = requests.get('https://monkagiga.firebaseio.com/.json')
-    return jsonify(response.text)
+    # response = requests.get('https://monkagiga.firebaseio.com/.json')
+
+    elasticsearch_connection = Elastic.get_elasticsearch_connection()
+    term = request.args.get('query')
+    
+    response = Query.query(term, elasticsearch_connection)
+    print(response)
+    return jsonify(response)
 
 
 if __name__ == '__main__':
