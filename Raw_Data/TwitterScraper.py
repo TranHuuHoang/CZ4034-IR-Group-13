77b7a34ffb379899f7560a3ce67ded07fff5a51a
kyrogex1 <43825328+kyrogex1@users.noreply.github.com>
2020-03-17 02:08:33 +0800

    Updated Scraper for non-truncated search

--- Raw_Data/TwitterScraper.py
+++ Raw_Data/TwitterScraper.py
@@ -2,18 +2,18 @@ import tweepy as tw
 import pandas as pd
 import time
 
-consumer_key = "YOUR API KEY HERE"
-consumer_secret = "YOUR API KEY HERE"
-access_token = "YOUR API KEY HERE"
-access_token_secret = "YOUR API KEY HERE"
+consumer_key = "VadtVXSmHFSAGJMOehPUBlEwY"
+consumer_secret = "fT7bVc7d4qU0G7EIddUIVHh6mdGCAsa3vyLxHCwbMGVApN2h4C"
+access_token = "2580718434-CdBur2K08VRxqp9IcOLey0TdryjRsV6IgSr4uZr"
+access_token_secret = "lWYkgidOtYXGGM5puXaxIbnmyKN7iMQezVDgh0Gp4ToFf"
 
 auth = tw.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tw.API(auth,wait_on_rate_limit=True, wait_on_rate_limit_notify = True)
 
 def scrape(query, count):
-    cur = tw.Cursor(api.search, q=query+"-filter:retweets", lang="en")
-    tweet_info = [(tweet.id_str, tweet.created_at, tweet.text, tweet.user.name, "@"+tweet.user.screen_name, str(tweet.in_reply_to_status_id)) for tweet in cur.items(count)]
+    cur = tw.Cursor(api.search, q=query+"-filter:retweets", lang="en", tweet_mode = "extended")
+    tweet_info = [(tweet.id_str, tweet.created_at, tweet.full_text, tweet.user.name, "@"+tweet.user.screen_name, str(tweet.in_reply_to_status_id)) for tweet in cur.items(count)]
     df = pd.DataFrame(tweet_info, columns = ["TweetID", "Date(SGT - 9)", "TweetText", "AuthorName", "UserHandle", "ReplyTweetID"])
     return df
 
