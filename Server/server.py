a46d1ae4eb3e0fa4d20c4b9fddb6481ff4c33998
Hoang <thhoang1111999@gmail.com>
2020-03-16 17:42:17 +0800

    Serve static files

--- Server/server.py
+++ Server/server.py
@@ -1,18 +1,21 @@
-from flask import Flask, render_template, url_for, jsonify, request
+from flask import Flask, render_template, url_for, jsonify, request, send_from_directory
 import requests
 
 app = Flask(__name__)
+
 
 @app.route("/search")
 @app.route("/")
 def search():
     return render_template('search.html')
 
+
 @app.route("/ajax", methods = ['POST', 'GET'])
 def ajax():
     print(dict(request.values))
     response = requests.get('https://monkagiga.firebaseio.com/.json')
     return jsonify(response.text)
+
 
 if __name__ == '__main__':
     app.run(debug = True)
